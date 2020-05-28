628a46c2b72d2e0cfa5ca2301b509b678ef109c2
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-05-28 21:14:46 +0900

    try cron

--- handler.py
+++ handler.py
@@ -1,11 +1,15 @@
 import os
+from datetime import datetime, timedelta, timezone
 from slack import WebClient
 from slack.errors import SlackApiError
 
 client = WebClient(token=os.environ['TFANDKUSU_SLACK_API_KEY'])
 
 def hello(event, context):
+    jst = timezone(timedelta(hours=9), 'JST')
+    now = datetime.now(jst)
+    timestr = now.strftime('%Y/%m/%d %H:%M:%S')
     response = client.chat_postMessage(
         channel='#random',
-        text="Hello world!")
+        text="Current time is " + timestr)
     return "Success"
