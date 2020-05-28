09eb04cdf2d92d2342f65ddb991f45f77fce1f9a
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-05-28 20:15:08 +0900

    Post to slack.

--- handler.py
+++ handler.py
@@ -1,24 +1,11 @@
-import json
+import os
+from slack import WebClient
+from slack.errors import SlackApiError
 
+client = WebClient(token=os.environ['TFANDKUSU_SLACK_API_KEY'])
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    response = client.chat_postMessage(
+        channel='#random',
+        text="Hello world!")
+    return "Success"
